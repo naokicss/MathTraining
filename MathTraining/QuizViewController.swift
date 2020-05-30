5e1f115e11920204884c246df0432a0f4345bfb6
naokihtml <miyaki101033@gmail.com>
2020-05-30 19:05:54 +0900

    問題を表示ページの途中までコード記述終わりました。

---
+++ MathTraining/QuizViewController.swift
@@ -0,0 +1,52 @@
+//
+//  QuizViewController.swift
+//  MathTraining
+//
+//  Created by みや on 2020/05/30.
+//  Copyright © 2020 みや. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+    
+    let total = 10 //問題数
+    var correct = 0 //正解数
+    var questionIndex = 0 //問題の記号
+    var answerIndex = 0 //正解の記号
+    
+    @IBOutlet var leftNumberLabel:UILabel!
+    @IBOutlet var centerNumberLabel:UILabel!
+    @IBOutlet var rightNumberLabel:UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    func setQuestions(){
+        let leftNum = Int(arc4random_uniform(10))
+        var centerNum = Int(arc4random_uniform(10))
+        
+        answerIndex = Int(arc4random_uniform(4))
+        
+        switch answerIndex {
+        case 0:
+            rightNumberLabel.text =  "\(leftNum + centerNum)"
+        case 1:
+            rightNumberLabel.text =  "\(leftNum - centerNum)"
+        case 2:
+            rightNumberLabel.text =  "\(leftNum * centerNum)"
+        default:
+            if centerNum == 0{
+                centerNum = 1
+            }
+            
+            rightNumberLabel.text =  "\(leftNum / centerNum)"
+        }
+         leftNumberLabel.text =  "\(leftNum)"
+         centerNumberLabel.text =  "\(centerNum)"
+    }
+}
